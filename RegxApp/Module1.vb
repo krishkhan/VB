78dbf35a6e791f6658886c0ecac4a9d978e7b007
vamc3132 <vamsikrishna.yesu@gmail.com>
2012-12-03 00:43:25 +0530

    added a new test

--- RegxApp/Module1.vb
+++ RegxApp/Module1.vb
@@ -2,7 +2,20 @@
 
 Module Module1
 
+    
+
     Sub Main()
+
+        Dim dictionary As New Dictionary(Of String, tagNameInformation)
+        Calculate(dictionary)
+
+
+        Dim pair As KeyValuePair(Of String, tagNameInformation)
+        For Each pair In dictionary
+            Console.WriteLine("{0} with {1} ,{2}", pair.Key, pair.Value.definitionName, pair.Value.elementName)
+        Next
+
+
         Dim values() As String = {"ABD0123", "ABC-", "A123456789ABCDEF"}
         ' Dim pattern As String = "^[A-Z][0-9]"
         'Dim pattern As String = "^((%%)?[A-Z][A-Z0-9_-]*)?$|^%Z\d{16}$"
@@ -16,5 +29,37 @@ Module Module1
             End If
         Next
     End Sub
+
+    Sub Calculate(ByRef dictionary As Dictionary(Of String, tagNameInformation))
+
+        Dim tag As New tagNameInformation
+        tag.definitionName = "Human"
+        tag.elementName = "carbon"
+        tag.typicalName = "vamson"
+        tag.loopName = "Marriage"
+
+
+
+        dictionary.Add("Vamsi1", tag)
+        tag.definitionName = "Human1"
+        tag.elementName = "carbon1"
+        tag.typicalName = "vamson1"
+        tag.loopName = "Marriage1"
+
+        dictionary.Add("home", tag)
+
+
+    End Sub
+
+End Module
+
+Module Utility
+    Public Structure tagNameInformation
+        Public typicalName As String
+        Public loopName As String
+        Public elementName As String
+        Public definitionName As String
+        Public tagName As String
+    End Structure
 End Module
 
