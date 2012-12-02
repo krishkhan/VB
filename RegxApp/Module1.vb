57a5bccc48705c72af05f2b9356b4adf17002939
vamc3132 <vamsikrishna.yesu@gmail.com>
2012-12-02 07:14:31 +0530

    Added new solution for reg exp

---
+++ RegxApp/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Text.RegularExpressions
+
+Module Module1
+
+    Sub Main()
+        Dim values() As String = {"ABD0123", "ABC-", "A123456789ABCDEF"}
+        ' Dim pattern As String = "^[A-Z][0-9]"
+        'Dim pattern As String = "^((%%)?[A-Z][A-Z0-9_-]*)?$|^%Z\d{16}$"
+        Dim pattern As String = "^((%%)?[A-Z][A-Z0-9]*)?$|^%Z\d{16}$"
+        For Each value As String In values
+            If Regex.IsMatch(value, pattern) Then
+                Console.WriteLine("{0} is a valid", value)
+            Else
+                Console.WriteLine("{0} is invalid", value)
+
+            End If
+        Next
+    End Sub
+End Module
+
---
+++ RegxApp/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RegxApp")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("RegxApp")> 
+<Assembly: AssemblyCopyright("Copyright ©  2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4c4262c2-1a96-4281-935e-b3c8bb875d78")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
